52c54cbc39052d40a03f4e19dae4d417e4fe353a
Chris Males <Chris.Males@jet2.com>
2025-05-12 10:40:54 +0100

    Add project files.

---
+++ HolidaySearch.Core/Domain/Holiday.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidaySearch.Core.Domain
+{
+    public class Holiday
+    {
+        public Flight Flight { get; set; }
+        public Hotel Hotel { get; set; }
+        public decimal TotalPrice => Flight.Price + (Hotel.PricePerNight * Hotel.Nights);
+    }
+}
---
+++ HolidaySearch.Core/Domain/Request/SearchRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidaySearch.Core.Domain.Request
+{
+    public class SearchRequest
+    {
+        public List<string> DepartingFrom { get; set; }
+        public string TravelingTo { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public int Duration { get; set; }
+    }
+}
