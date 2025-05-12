3179b7f68771bf3fc33e4c0b74aaabd3d2d238b5
Chris Males <Chris.Males@jet2.com>
2025-05-12 11:33:31 +0100

    Add bare minimum hotel search functionality and failing unit test along with hotels.json data.

--- HolidaySearch.Core/Application/Services/HotelSearchService.cs
+++ HolidaySearch.Core/Application/Services/HotelSearchService.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using HolidaySearch.Core.Domain.Request;
+using HolidaySearch.Core.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HolidaySearch.Core.Application.Interfaces;
 
 namespace HolidaySearch.Core.Application.Services
 {
-    public class HotelSearchService
+    public class HotelSearchService: IHotelSearchService
     {
+        private readonly IEnumerable<Hotel> _hotels;
+
+        public HotelSearchService(IEnumerable<Hotel> hotels)
+        {
+            _hotels = hotels;
+        }
+
+        public IEnumerable<Hotel> Search(SearchRequest request)
+        {
+            return Enumerable.Empty<Hotel>();
+        }
     }
 }
---
+++ HolidaySearch.Tests/Services/HotelSearchServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using HolidaySearch.Core.Application.Services;
+using HolidaySearch.Core.Domain;
+using HolidaySearch.Core.Domain.Request;
+
+namespace HolidaySearch.Tests.Services
+{
+    public class HotelSearchServiceTests
+    {
+        [Test]
+        public void ShouldReturnHotelMatchingArrivalDateAirportAndDuration()
+        {
+            var hotels = new List<Hotel>
+            {
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Nh Malaga",
+                    ArrivalDate = new DateTime(2023, 7, 1),
+                    Nights = 7,
+                    PricePerNight = 90,
+                    LocalAirports = new List<string> { "AGP" }
+                }
+            };
+
+            var request = new SearchRequest
+            {
+                DepartureDate = new DateTime(2023, 7, 1),
+                TravelingTo = "AGP",
+                Duration = 7
+            };
+
+            var service = new HotelSearchService(hotels);
+            var results = service.Search(request).ToList();
+
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual(1, results.First().Id);
+        }
+    }
+}
